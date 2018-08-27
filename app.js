61dd4d8682a297df0d80be74e9fa58018e9b22f7
dhaval <dhavi78@gmail.com>
2018-08-27 09:44:09 +0530

    make it better

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080
 var loginController = require('./controller/loginController');
 app.set('view engine','ejs');
 app.use(express.static('./public'));
 loginController(app);
-app.listen(3000);
+app.listen(8080 , function(){
+    console.log("application running")
+});
